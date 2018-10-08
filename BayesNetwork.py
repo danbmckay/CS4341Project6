967df7e5c524b0f3403cd63131c82205279689f1
Jack <msgonsalves@wpi.edu>
2018-10-08 03:00:13 -0400

    weight setups are done for the correct type of search

--- BayesNetwork.py
+++ BayesNetwork.py
@@ -7,7 +7,7 @@ class Node():
     def __init__(self, node_name, some_parents, some_conditions):
 
         self.node_name = node_name
-        self.node_place = int(node_name.isdigit()) - 1
+        self.node_place = int(''.join(i for i in node_name if i.isdigit())) - 1
         self.weight = 0
         self.weight_added = False
         if len(some_conditions) > 1:
@@ -20,7 +20,10 @@ class Node():
         self.num_children = 0
         self.status = False
         self.been_checked = False
-        self.conditions = some_conditions
+        self.conditions = []
+        for a_condition in some_conditions:
+            self.conditions.append(float(a_condition))
+
         self.num_parents = len(some_parents)
 
     # do after all nodes have been created to find their parent nodes
@@ -47,34 +50,38 @@ class Node():
     def make_weight(self):
         # don't redo work, checks if it has been done
         if not self.weight_added:
+            # if the node is just a parent node the weight is equal to the value of the probability its true
             if not self.has_parents:
                 self.weight_added = True
                 self.weight = self.conditions[0]
 
             else:
+                # make sure parent weights are set first other wise there will be problems
                 for a_parent in self.parent_nodes:
                     a_parent.make_weight()
 
+                # get the number of layers that are going to be in the table
                 num_true = 1
                 for i in range(0, len(self.parent_nodes)):
                     num_true *= 2
 
+                # get rows of probabilities to multiply and add together
                 indiv_weights = []
                 for i in range(0, num_true):
-                    temp_string = []
-                    temp_nums = [self.conditions[i]]
+                    temp_string = []  # check the string being looked at
+                    temp_nums = [self.conditions[i]] # the string of probabilities to be multiplied together
                     temp_counter = 1
+                    # get the all the correct probabilities from parents for all permutations
                     for a_parent in self.parent_nodes:
-                        trulse = int(i/temp_counter)%2
+                        trulse = int(i/temp_counter) % 2  # gets a 0 1 in the correct spot
                         temp_string.append(trulse)
                         temp_nums.append(a_parent.get_weight(trulse))
                         temp_counter *= 2
-                    print(temp_string)
                     indiv_weights.append(temp_nums)
 
                 the_weight = 0
                 for a_weight in indiv_weights:
-                    temp_weight = 0
+                    temp_weight = 1
                     for a_num in a_weight:
                         temp_weight *= a_num
                     the_weight += temp_weight
@@ -109,23 +116,29 @@ class Node():
     #         elif self.status == 0:
     #             return [1 - self.conditions[0], False]
 
+    # calculates a random outcome of something happening
     def happened(self):
 
         if self.has_parents:
+            # again don't duplicate answers
             if not self.been_checked:
-                parents_happend = []
+                parents_happened = []
                 for a_parent in self.parent_nodes:
-                    parents_happend.append(a_parent.happend())
+                    # make sure that the parents have some result already
+                    parents_happened.append(a_parent.happend())
 
+                    # get some random number
                     choice = random.uniform(0, 1)
                     temp_place = 0
                     counter = 1
-                    for a_parent in parents_happend:
-                        if a_parent:
+                    # find the correct location in the truth table to compare later
+                    for some_parent in parents_happened:
+                        if some_parent:
                             temp_place += counter
 
                         counter *= 2
 
+                    # determine if it happened or not
                     a_prob = self.conditions[temp_place]
                     if a_prob > choice:
                         self.status = True
@@ -133,11 +146,15 @@ class Node():
                     else:
                         self.status = False
                         return False
+            # if its already been done, return whats already there
             else:
                 return self.status
 
+        # check nodes that are just parents
         else:
+            # don't get multiple answers, just do it once
             if not self.been_checked:
+                # get a random number and make a choice based on the likely hood of being right
                 choice = random.uniform(0, 1)
                 if self.conditions[0] > choice:
                     self.status = True
@@ -199,6 +216,8 @@ def main():
     for node in node_list:
         print(node.node_place)
         node.find_parents(node_list)
+        node.make_weight()
+        print(node.weight)
 
 
 if __name__ == "__main__":
